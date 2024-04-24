0538f279833e6d1328b1d6cca08c3d7ca5feecc8
Gabriel Pimenta <gabrielrpimenta@gmail.com>
2024-04-24 17:51:35 -0300

    [FEAT]: eslint and prettier config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-transform-flow-strip-types'],
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.jsx', '.js', '.json', '.svg'],
+        },
+      ],
+    ],
   };
 };
